7671fde10d34fa6ac4fcae736338b84d845eced1
imirica <iulivasilemirica@gmail.com>
2021-08-13 15:59:52 +0300

    appBringup is ready

---
+++ appBringup.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+#Create a Label Widget
+myLabel = Label(root, text="DuplicateFinder App")
+#Shoving the Label Widget onto the screen
+myLabel.pack()
+
+root.mainloop()
